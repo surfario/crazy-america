1311675d63bec6dc961472472b19d2f2ff3b6ae8
Ari Olmos <ari.olmos@gmail.com>
2014-06-23 08:44:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrazyAmerica::Application.config.secret_key_base = '1bae3ffda6a04a53c6166a4a3327114489747ff1b4496a2cc907b38289390968a70e51761577e29e8567cb8a1e534fe0e85c885270392f6f5868e8d81b84e06c'
